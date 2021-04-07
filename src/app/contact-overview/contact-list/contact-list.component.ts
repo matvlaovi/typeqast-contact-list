feecedcb14219a0ac6f91a9c0258a2d782e69eee
Matej Vlaović <matej.vlaovic@gmail.com>
2021-04-07 20:58:29 +0200

    main list design, layout

--- src/app/contact-overview/contact-list/contact-list.component.ts
+++ src/app/contact-overview/contact-list/contact-list.component.ts
@@ -1,20 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {trigger, transition, style, animate, query, stagger} from '@angular/animations';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
     selector: 'app-contact-list',
     templateUrl: './contact-list.component.html',
-    styleUrls: ['./contact-list.component.scss']
+    styleUrls: ['./contact-list.component.scss'],
 })
-export class ContactListComponent implements OnInit {
+export class ContactListComponent implements OnInit, OnChanges {
 
     @Input() showFavorites: boolean;
 
     public searchTerm: string = '';
 
-    public data = ['test1', 'test2', 'test3', 'test4', 'test5',
-        'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12', 'test13', 'test14']
+    public data1 = ['test1', 'Addie Hernandez', 'Oscar Arnold', 'Neki Random Lik', 'test5',
+        'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12', 'test13', 'test14'];
+
+    public data: any[];
 
     constructor() { }
+
+    ngOnChanges(): void {
+        if (this.showFavorites) {
+            this.data = this.data1.slice(10);
+        } else {
+            this.data = this.data1;
+        }
+    }
 
     ngOnInit(): void {
     }
